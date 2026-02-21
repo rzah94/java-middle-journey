5a1f4f9ece22a7b86a6c7fd10da32ebf5a9e2b49
rzah <rzah94@yandex.ru>
2026-02-21 12:11:56 +0300

    add correct equals implementation

--- src/main/java/week1/core/equals/Main.java
+++ src/main/java/week1/core/equals/Main.java
@@ -1,4 +1,32 @@
 package week1.core.equals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
+    public static void main(String[] args) {
+        scenario1();
+
+
+    }
+
+    public static void scenario1() {
+        Set<User> users = new HashSet<>();
+
+        User user1 = new User(1L, "1@1.ru");
+        User user2 = new User(2L, "1@1.ru");
+
+        users.add(user1);
+        users.add(user2);
+
+        /* Добавлены оба объекта, т.к. hashcode() не переопределен. Используется hashcode() из
+        класса Object. Из-за этого у объектов разный hash и HashSet оба их добавил.
+
+        HashSet - это HashMap (типа map, но ключи (корзины) - это hash), только с уникальными значениями.
+        Внутри каждой корзины Node<K, V>.
+         */
+        System.out.println(users.contains(user1));
+        System.out.println(users.contains(user2));
+        System.out.println(users.size());
+    }
 }
--- src/main/java/week1/core/equals/User.java
+++ src/main/java/week1/core/equals/User.java
@@ -1,4 +1,34 @@
 package week1.core.equals;
 
+import java.util.Objects;
+
 public class User {
+    private final Long id;
+    private final String email;
+
+    public User(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
 }
